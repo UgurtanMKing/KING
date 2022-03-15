cdf1dcc188368672a873ba27e15b6f4c06279c87
Uğurtan Mustafa <ugurtanmustafakaraoglu@gmail.com>
2022-03-15 18:26:26 +0300

    website

---
+++ Models/odev.cs
@@ -0,0 +1,10 @@
+using System;
+public class director{
+public string ad {get; set;}=null!;
+public string soyad {get; set;}=null!;
+public DateTime yil {get;set;}
+public double carpma {get;set;}
+public bool tf {get;set;}
+public int kod{get;set;}
+public decimal sayi1{get;set;}
+}   
