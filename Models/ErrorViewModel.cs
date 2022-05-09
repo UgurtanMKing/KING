581caec7083f44ac142f445ac9a560860031e2d3
Uğurtan Mustafa <101579803+UgurtanMKing@users.noreply.github.com>
2022-05-09 18:08:35 +0300

    Add files via upload

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -1,8 +1,11 @@
-namespace King.Models;
+using System;
 
-public class ErrorViewModel
+namespace ADMİN.Models
 {
-    public string? RequestId { get; set; }
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
 
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
 }
